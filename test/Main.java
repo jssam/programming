9bab4827eb24a8eb2c12da8dd6c2d7613b519a60
jssam <jains3099@gmail.com>
2021-04-30 03:23:51 +0530

    main

---
+++ test/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        Long num = scn.nextLong();
+        for(int n =0;n<num;n++){
+            Long x = scn.nextLong();
+            Long y = scn.nextLong();
+            Long r = scn.nextLong();
+            Long rupee = 0L;
+            Long stx=1L;
+            Long sty=1L;
+            if(x==0&&y==0){System.out.println("NO"); continue;}
+            while(stx!=x||sty!=y){
+                if(stx!=x){
+                    stx++;
+                    rupee +=sty;
+                }
+                if(sty!=y){
+                    sty++;
+                    rupee +=stx;
+                    System.out.println("re"+rupee);
+                }
+                if(rupee>r){
+                    break;
+                }
+            }
+            System.out.println("fi"+rupee);
+            if(rupee==r){
+                System.out.println("YES");
+            }else{System.out.println("NO");}
+            
+        }        
+     
+    }
+}
+    
